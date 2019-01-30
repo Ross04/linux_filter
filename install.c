19bf5e85a5573c7b43a6cf669a79d9fc5af3dc30
Ross04 <rosswiebe14@gmail.com>
2019-01-30 17:43:43 -0600

    Added user interface for proxy location and port

--- install.c
+++ install.c
@@ -1,18 +1,29 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 int main () {
     char command[100];
+    char ip[20];
+    char novarcmd[100];
+    char novarcmd0[100];
+    char novarcmd1[100];
+
+    printf("Welcome to Linux_filter.  Please enter proxy ip address and port in this format: xxx.xxx.xxx.xxx:XXXX:\n");
+    scanf(" %s", ip);
 
     strcpy( command, "sudo iptables -F" );
     system(command);
-    strcpy( command, "echo '1' > /proc/sys/net/ipv4/ip_forward" );
+    strcpy( command, "sudo echo '1' > /proc/sys/net/ipv4/ip_forward" );
     system(command);
-    strcpy( command, "sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination 192.168.1.9:8080" );
+    strcpy( novarcmd, "sudo iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination " );
+    strcpy( command, strcat( novarcmd, ip ));
     system(command);
-    strcpy( command, "sudo iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination 192.168.1.9:8080" );
+    strcpy( novarcmd0, "sudo iptables -t nat -A OUTPUT -p tcp --dport 443 -j DNAT --to-destination " );
+    strcpy( command, strcat( novarcmd0, ip ));
     system(command);
-    strcpy( command, "sudo iptables -t nat -A OUTPUT -p udp --dport 443 -j DNAT --to-destination 192.168.1.9:8080" );
+    strcpy( novarcmd1, "sudo iptables -t nat -A OUTPUT -p udp --dport 443 -j DNAT --to-destination " );
+    strcpy( command, strcat( novarcmd1, ip ));
     system(command);
 
     return(0);
