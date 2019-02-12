42b30db3ef88d7dab581779308f9ea26b6df5f80
Ross04 <rosswiebe14@gmail.com>
2019-02-12 16:33:08 -0600

    added comments

--- install.c
+++ install.c
@@ -4,6 +4,7 @@
 #include <unistd.h>
 
 int main () {
+    /* declare variables */
     char command[100];
     char ip[25];
     char novarcmd[100];
@@ -12,23 +13,30 @@ int main () {
 
     FILE *fp;
 
+    /* check if file .filterdata exists */
     if ( access ( "/usr/local/bin/.filterdata", F_OK ) != -1 ) {
+	/* open and read the file */
 	fp = fopen("/usr/local/bin/.filterdata", "r");
 	fgets(ip, 25, (FILE*)fp);
 	fclose(fp);
     } else {
+	/* print request for proxy data from user (when not found in file). */
 	printf("Welcome to Linux_filter.  Please enter proxy ip address and port in this format: xxx.xxx.xxx.xxx:XXXX:");
 	scanf(" %s", ip);
 	printf("\n");
+	/* and write it to a file */
 	fp = fopen("/usr/local/bin/.filterdata", "w+");
 	fprintf(fp, "%s\n", ip);
 	fclose(fp);
     }
 
+    /* actually use the data starting by flushing iptable nat (currently not working). */
     strcpy( command, "iptables -t nat -F" );
     system(command);
+    /* enable forwarding */
     strcpy( command, "echo '1' > /proc/sys/net/ipv4/ip_forward" );
     system(command);
+    /* next few lines set proxy */
     strcpy( novarcmd, "iptables -t nat -A OUTPUT -p tcp --dport 80 -j DNAT --to-destination " );
     strcpy( command, strcat( novarcmd, ip ));
     system(command);
